a5b246f7bfafd58969f609f58e9b7213ce1e64f9
bretlee492 <bretlee.privett@student.oneidaschools.org>
2024-08-16 17:04:08 +0000

    initial commit.

---
+++ variable.py
@@ -0,0 +1,12 @@
+message = "hello python world!9876"
+print(message)
+message = "python is my favorite language!9876"
+print(message)
+message = "Thank you for sharing Python with the world, Guido!"
+print(message)
+message = "I love welding"
+print(message)
+my_string = "this is a double qouted string."
+print(message)
+my_string = "this is a single qouted string."
+print(message)

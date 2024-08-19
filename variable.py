e3383cf4018e119c64a1cabb4accbb92a2e2cbdf
bretlee492 <bretlee.privett@student.oneidaschools.org>
2024-08-19 17:06:32 +0000

    inital commit.

--- variable.py
+++ variable.py
@@ -10,3 +10,19 @@ my_string = "this is a double qouted string."
 print(message)
 my_string = "this is a single qouted string."
 print(message)
+quote = "Linus Torvalds once said, 'Any program is only as good as it is useful. '"
+print(message) 
+first_name = 'Bretlee'
+print(message)
+print(first_name)
+print(message)
+print(first_name.title())
+print(message)
+print(first_name.upper())
+print(message)
+print(first_name.lower())
+print(message)
+first_name = 'bretlee'
+print(message)
+last_name = 'privett'
+print(message)

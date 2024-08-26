c18dedc493d9e987df0c6e0d910f3a6e7574e7cf
bretlee492 <bretlee.privett@student.oneidaschools.org>
2024-08-26 16:38:01 +0000

    update commit.

--- variable.py
+++ variable.py
@@ -31,3 +31,70 @@ print(message)
 print(full_name.title())
 print(message)
 first_name = 'bretlee'
+last_name = 'Privett'
+print(message)
+full_name = first_name + ' ' + last_name
+print(message)
+message = full_name.title() + ' ' + "was considered the world's first computer programmer."
+print(message)
+print("Hello everyone!")
+print(message)
+print("\tHello everyone!")
+print(message)
+print("Hello \teveryone!")
+print(message)
+print("Hello everyone!")
+print(message)
+print("\nHello everyone!")
+print(message)
+print("Hello \neveryone!")
+print(message)
+print("\n\n\nHello everyone!")
+print(message)
+name = ' bretlee '
+print(message)
+print(name.lstrip())
+print(message)
+print(name.rstrip())
+print(message)
+print(name.strip())
+print(message)
+name = 'bretlee '
+print(message)
+print('-' + name.lstrip() + '-')
+print(message)
+print('-' + name.rstrip() + '-')
+print(message)
+print('-' + name.strip() + '-')
+print(message)
+print(3+2)
+print(message)
+print(3-2)
+print(message)
+print(3*2)
+print(message)
+print(3/2)
+print(message)
+print(3**2)
+print(message)
+standard_order = 2+3*4
+print(standard_order)
+my_order = (2+3)*4
+print(my_order)
+print(0.1+0.1)
+print(0.1+0.2)
+print(3*0.1)
+# Python 2.7
+print (4/2)
+# Python 2.7
+print (3/2)
+# Python 3.3
+print(4/2)
+# Python 3.3
+print(3/2)
+# This line is a comment. 
+print("This line is not a comment, it is code.")
+import this
+# I learned how to strip whitespace from strings.
+name = '\t\teric'
+print("I can strip tabs from my name: " + name.strip())

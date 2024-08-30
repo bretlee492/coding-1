2d1edd44b5f924f727dbec0388ca2af94ec22f55
bretlee492 <bretlee.privett@student.oneidaschools.org>
2024-08-30 16:16:01 +0000

    update commit.

--- lists.py
+++ lists.py
@@ -13,23 +13,16 @@ print(dog)
 dog = dogs[1]
 print(dog)
 dog = dogs[-1]
-print(dog)
-
-#dog = dogs[1]
-#print= (dog.title())
-#dogs = ['German shorthaired pointer', 'Labrador retreiver', 'English cockerspaniel']
-#dogs = dogs[-1]
-#print= (dog.title())
-#dogs = ['German shorthaired pointer', 'Labrador retreiver', 'English cockerspaniel']
-#dogs = dogs[-2]
-#print= (dog.title())
-#dogs = ['German shorthaired pointer', 'Labrador retreiver', 'English cockerspaniel']
-#dogs = dogs[-3]
-#print= (dog.title())
-#dogs = ['German shorthaired pointer', 'Labrador retreiver', 'English cockerspaniel']
-#for g in dogs:
-#    print(g.title())
-
-
-
-
+print(dog.title())
+dogs = ['German shorthaired pointer', 'Labrador retreiver', 'English cockerspaniel']
+dog = dogs[-2]
+print(dog.title())
+dogs = ['German shorthaired pointer', 'Labrador retreiver', 'English cockerspaniel']
+dog = dogs[-3]
+print(dog.title())
+dogs = ['German shorthaired pointer', 'Labrador retreiver', 'English cockerspaniel']
+for dog in dogs:
+    print(dog)
+dogs = ['German shorthaired pointer', 'Labrador retreiver', 'English cockerspaneil']
+for dog in dogs:
+    print('I like ' + dog + "s.")

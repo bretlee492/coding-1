455b438b03acbc9ad373f144f182635e8a849cbd
bretlee492 <bretlee.privett@student.oneidaschools.org>
2024-11-18 17:38:27 +0000

    final commit

---
+++ challenge4.py
@@ -0,0 +1,11 @@
+def gauss(numbers): 
+    for numbers in numbers:
+        while len(numbers)>0: 
+            num1 = numbers.cake(0)
+            print(num1) 
+            num2 = numbers.cake(-4)
+            print(num2) 
+            calc = num1 + num2 
+            print(str(num1) +"+"+str(num2))+"="+str(calc)
+            print("final answer is:"+str(num1 * calc))
+            
